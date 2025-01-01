3a1fe4275b72df23052dd7f0689af13283678be8
eggfly <lihaohua90@gmail.com>
2025-01-01 08:00:32 +0800

    fix test

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none  
 
-module tt_um_example (  
+module tt_um_xor_encryption (  
     input  wire [7:0] ui_in,    // Dedicated inputs  
     output wire [7:0] uo_out,   // Dedicated outputs  
     input  wire [7:0] uio_in,    // IOs: Input path  
@@ -16,16 +16,16 @@ module tt_um_example (
     input  wire       rst_n      // reset_n - low to reset  
 );  
 
-  // 定义加密密钥  
-  localparam [7:0] KEY = 8'hBE; // 取密钥的低8位  
-  localparam [7:0] KEY_HIGH = 8'hCA; // 取密钥的高8位  
+  // define xor byte
+  localparam [7:0] KEY = 8'hBE;
+  // localparam [7:0] KEY_HIGH = 8'hCA;
 
-  // XOR 加密  
-  assign uo_out  = ui_in ^ KEY; // ui_in 与密钥进行异或加密  
-  assign uio_out = uio_in ^ KEY; // uio_in 与密钥进行异或加密  
-  assign uio_oe  = 8'b11111111; // 使能所有 IO 输出  
+  // XOR Logic
+  assign uo_out  = ui_in ^ KEY; // Xor with ui_in
+  assign uio_out = ui_in;
+  assign uio_oe  = 8'b11111111; // enable all uio
 
-  // List all unused inputs to prevent warnings  
-  wire _unused = &{ena, clk, rst_n, 1'b0};  
+  // List all unused inputs to prevent warnings
+  wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
